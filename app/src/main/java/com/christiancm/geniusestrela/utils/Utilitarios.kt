12e85d4971cbf91a0c82590a0733dd975fc99a25
ChristianCMM <chriis.mattos@gmail.com>
2025-09-02 21:49:58 -0300

    Subindo projeto Inicial

---
+++ app/src/main/java/com/christiancm/geniusestrela/utils/Utilitarios.kt
@@ -0,0 +1,49 @@
+package com.christiancm.geniusestrela.utils
+
+import android.content.Context
+import androidx.core.content.ContextCompat
+import com.christiancm.geniusestrela.R
+
+class Utilitarios {
+
+    fun gerarNumeroAleatorio():Int{
+        val numero = (0..3).random()
+        return numero
+    }
+
+    companion object {
+
+        fun whiteRed(context: Context): Int {
+            return ContextCompat.getColor(context, R.color.white_red)
+        }
+
+        fun whiteBlue(context: Context): Int {
+            return ContextCompat.getColor(context, R.color.white_blue)
+        }
+
+        fun whiteGreen(context: Context): Int {
+            return ContextCompat.getColor(context, R.color.white_green)
+        }
+
+        fun whiteYellow(context: Context):Int{
+            return ContextCompat.getColor(context, R.color.white_yellow)
+        }
+
+
+        fun redColor(context: Context): Int {
+            return ContextCompat.getColor(context, R.color.red)
+        }
+
+        fun blueColor(context: Context): Int {
+            return ContextCompat.getColor(context, R.color.blue)
+        }
+
+        fun greenColor(context: Context): Int {
+            return ContextCompat.getColor(context, R.color.green)
+        }
+
+        fun yellowColor(context: Context):Int{
+            return ContextCompat.getColor(context, R.color.yellow)
+        }
+    }
+}
